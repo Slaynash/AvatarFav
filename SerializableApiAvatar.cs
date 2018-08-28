5dc46d7c8ec2ffb5b490ff3c09b14bb60a31c7a6
Slaynash <slaynash@survival-machines.fr>
2018-08-29 01:49:13 +0200

    removed old code from SerializableApiAvatar

--- SerializableApiAvatar.cs
+++ SerializableApiAvatar.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 namespace AvatarFav
 {
-    [Serializable]
     public class SerializableApiAvatar
     {
         public string id;
@@ -19,43 +18,6 @@ namespace AvatarFav
 		public double version;
         public string unityPackageUrl;
         public string thumbnailImageUrl;
-
-        public SerializableApiAvatar(string id, string name, string imageUrl, string authorName, string authorId, string assetUrl, string description, List<string> tags, int version, string unityPackageUrl, string thumbnailImageUrl)
-        {
-            this.id = id;
-            this.name = name;
-            this.imageUrl = imageUrl;
-            this.authorName = authorName;
-            this.authorId = authorId;
-            this.assetUrl = assetUrl;
-            this.description = description;
-            this.tags = tags.ToArray();
-            this.version = version;
-            this.unityPackageUrl = unityPackageUrl;
-            this.thumbnailImageUrl = thumbnailImageUrl;
-        }
-
-        public Dictionary<string, object> getDictionary()
-        {
-            Dictionary<string, object> dic = new Dictionary<string, object>();
-
-            dic.Add("id", id);
-            dic.Add("name", name);
-            dic.Add("imageUrl", imageUrl);
-            dic.Add("authorName", authorName);
-            dic.Add("authorId", authorId);
-            dic.Add("assetUrl", assetUrl);
-            dic.Add("description", description);
-            List<object> tagList = new List<object>();
-            tagList.AddRange(tags);
-            dic.Add("tags", tagList);
-            dic.Add("version", version);
-            dic.Add("unityPackageUrl", unityPackageUrl);
-            dic.Add("thumbnailImageUrl", thumbnailImageUrl);
-            dic.Add("releaseStatus", "public");
-
-            return dic;
-        }
 
         public static List<SerializableApiAvatar> ParseJson(String json)
         {
@@ -71,37 +33,6 @@ namespace AvatarFav
                 saa.Add(JsonUtility.FromJson<SerializableApiAvatar>(m.Value));
             }
             return saa;
-        }
-
-        public string AsJson()
-        {
-
-            string tagsS = "[";
-            bool first = true;
-            foreach(string tag in tags)
-            {
-                if (!first) tagsS += ",";
-                tagsS += "\"" + tag.Replace("\"", "\\\"") + "\"";
-                first = false;
-            }
-            tagsS += "]";
-
-            string jsonString =
-                "{" +
-                    "\"id\":\"" + id + "\"," +
-                    "\"name\":\"" + name.Replace("\"", "\\\"") + "\"," +
-                    "\"imageUrl\":\"" + imageUrl + "\"," +
-                    "\"authorName\":\"" + authorName.Replace("\"", "\\\"") + "\"," +
-                    "\"authorId\":\"" + authorId + "\"," +
-                    "\"assetUrl\":\"" + assetUrl + "\"," +
-                    "\"description\":\"" + description.Replace("\"", "\\\"") + "\"," +
-                    "\"tags\":" + tagsS + "," +
-                    "\"version\":\"" + version + "\"," +
-                    "\"unityPackageUrl\":\"" + unityPackageUrl + "\"," +
-                    "\"thumbnailImageUrl\":\"" + thumbnailImageUrl + "\"" +
-                "}";
-
-            return jsonString;
         }
     }
 }
